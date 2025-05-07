04c7ac404a7189b6d5159e0ad8bae5147775ac6c
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-05-07 17:59:53 -0400

    First Component

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import InterpolacionCadenas from './components/InterpolacionCadenas.vue';
+</script>
+
+<template>
+  <div>
+    <InterpolacionCadenas />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/InterpolacionCadenas.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+let cuenta = 1;
+</script>
+
+<template>
+  <div>
+    <h1>Hola desde el componente Interpolación de Cadenas</h1>
+    <p>
+        {{ cuenta }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
