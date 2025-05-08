b892bd083a1ddcbb5a0f8b9da62c591ee110b0c7
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-05-07 21:22:56 -0400

    Bootstrap has been added, Baiding with class

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import InterpolacionCadenas from './components/InterpolacionCadenas.vue';
 import PropertyBinding from './components/PropertyBinding.vue';
+import PropertyBindingClass from './components/PropertyBindingClass.vue';
 </script>
 
 <template>
   <div>
     <!-- <InterpolacionCadenas /> -->
-     <PropertyBinding />
+     <!-- <PropertyBinding /> -->
+      <PropertyBindingClass/>
   </div>
 </template>
 
---
+++ src/components/PropertyBindingClass.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let isPrimary = ref(true);
+let isSamll = ref(true);
+
+const clasesBoton = ref({
+    'btn-primary': true,
+    'btn-sm': true
+})
+
+setTimeout(() => {
+    isPrimary.value = false;
+    isSamll.value = false;
+    clasesBoton.value['btn-primary'] = !clasesBoton.value['btn-primary'];
+    clasesBoton.value['btn-sm'] = !clasesBoton.value['btn-sm'];
+}, 2500);
+</script>
+
+<template>
+  <div>
+    <h1>Hello con Vue and Bootstrap</h1>
+    <hr><hr>
+    <button class="btn" :class="clasesBoton">
+        Botón Principal
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
