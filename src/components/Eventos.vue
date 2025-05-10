6f80f09f750094159502becdc98da12a99d9c7a0
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-05-10 00:21:36 -0400

    Modifiers, exact and of the mouse

--- src/components/Eventos.vue
+++ src/components/Eventos.vue
@@ -16,6 +16,16 @@ function cambiarNombre(event:KeyboardEvent){
   console.log('Tecla Presionada', tecla.key);
   
 }
+function dangerousFunctions(){
+  console.error("Calling dangerous function");
+}
+
+function functionClickDerecho(){
+  console.info('Function called with the mouse right click');
+}
+
+const functionEjecutadaUnaVez = () => console.log('Una vez');
+
 
 </script>
 
@@ -28,13 +38,22 @@ function cambiarNombre(event:KeyboardEvent){
     <h2>Name: {{ user.nombre }}</h2>
   </div> -->
   <div class="container">
-   <!-- <form action="" @submit.prevent.stop="guardar"> -->
-   <form @submit="guardar">
+   <form action="" @submit.prevent.stop="guardar">
+   <!-- <form @submit="guardar"> -->
     <div class="mb-3">
       <label for="nombre" class="form-label">Nombre</label>
-      <input type="text" name="nombre" id="" class="form-control" @keydown.enter.prevent="cambiarNombre($event)">
+      <!-- <input type="text" name="nombre" id="" class="form-control" @keydown.enter.prevent="cambiarNombre($event)"> -->
+      <input type="text" name="nombre" id="" class="form-control" @keydown.enter="cambiarNombre($event)">
     </div>
-    <button class="btn btn-primary">Enviar</button>
+    <button class="btn btn-primary" type="submit">Enviar</button>
+    <hr>
+    <button class="btn btn-danger btn-lg" @click.ctrl.alt.exact.prevent="dangerousFunctions" type="button">
+      DANGER (Only activate with Ctrl + Alt+ Click)
+    </button>
+    <hr>
+    <button class="btn btn-secondary" type="button" @click.right.prevent.stop="functionClickDerecho">Llamar con el click derecho</button>
+    <hr>
+    <button class="btn btn-success" type="button" @click.once="functionEjecutadaUnaVez">Ejecutar solo una vez</button>
    </form> 
   </div>
 </template>
