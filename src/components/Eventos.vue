bb46aa5f274a89f4580f1d1ec547b418bbc9dd7a
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-05-10 00:02:53 -0400

    Modifiers, de teclado

--- src/components/Eventos.vue
+++ src/components/Eventos.vue
@@ -1,23 +1,41 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const guardar = () => {
-    console.log("save");
-};
-
 const user = ref({
     id: 1,
     nombre: 'Chad Smith'
 });
+
+const guardar = () => {
+    console.log("save");
+};
+
+function cambiarNombre(event:KeyboardEvent){
+  console.log(event);
+  const tecla = (event as KeyboardEvent);
+  console.log('Tecla Presionada', tecla.key);
+  
+}
+
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <button class="btn btn-success btn-lg" v-on:click="guardar">Guardar</button>
     <hr>
     <button class="btn btn-success btn-lg" @click="user.nombre='Renzo Roman'">Change Name</button>
     <hr>
     <h2>Name: {{ user.nombre }}</h2>
+  </div> -->
+  <div class="container">
+   <!-- <form action="" @submit.prevent.stop="guardar"> -->
+   <form @submit="guardar">
+    <div class="mb-3">
+      <label for="nombre" class="form-label">Nombre</label>
+      <input type="text" name="nombre" id="" class="form-control" @keydown.enter.prevent="cambiarNombre($event)">
+    </div>
+    <button class="btn btn-primary">Enviar</button>
+   </form> 
   </div>
 </template>
 
